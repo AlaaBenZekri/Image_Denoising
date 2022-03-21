ff852da72d5d1322c8623bcb08713feac04fad0b
AlaaBenZekri <91311585+AlaaBenZekri@users.noreply.github.com>
2022-03-22 00:51:56 +0100

    Update data.py

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ from torch.autograd import Variable
 from torch.utils.data import random_split
 from torchvision import datasets, transforms
 import os
-
+# You can download the data from this link : https://www.kaggle.com/datasets/greatgamedota/ffhq-face-data-set
 def prepare_data(path, split_size=0.8, batch_size=16):
     transform = transforms.Compose([transforms.Resize(255),
                                 transforms.ToTensor()
